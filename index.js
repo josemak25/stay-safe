9a809b1685f5c93c5f4b685d8b4ec304d35d54e8
josemak25 <amakirij@gmail.com>
2020-04-14 11:34:43 +0100

    add fix for responsive menu and add scroll up icon size based of window screen

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const scrollUpButton = document.querySelector(".scroll_up");
+const pageContainer = document.querySelector(".container");
+const menu = document.querySelector(".menu");
+const menuButtons = document.querySelector(".hamburger_menu");
+const menuListItem = menu.children[0].children;
 
-window.addEventListener("scroll", (e) => {
+const pageHeight = pageContainer.scrollHeight - 1000;
+
+// HANDLE MENU EVENT
+const [openMobileMenu, closeMobileMenu] = menuButtons.children;
+
+openMobileMenu.addEventListener("click", () => {
+  menu.style.display = "flex";
+  openMobileMenu.style.display = "none";
+  closeMobileMenu.style.display = "flex";
+});
+
+const closeMobileMethod = () => {
+  menu.style.display = "none";
+  closeMobileMenu.style.display = "none";
+  openMobileMenu.style.display = "flex";
+};
+
+closeMobileMenu.addEventListener("click", closeMobileMethod);
+
+if (pageContainer.clientWidth <= 1024) {
+  Array.from(menuListItem).forEach((item) => {
+    item.addEventListener("click", closeMobileMethod);
+  });
+}
+
+// HANDLE SCROLL EVENT
+window.addEventListener("scroll", () => {
   const fromTop = window.scrollY;
 
   if (fromTop >= 600) return (scrollUpButton.style.display = "flex");
